821d3e3a9f1c5370a453c5fd27dd41dfeee480b4
mpotane <1000100ten@gmail.com>
2022-09-28 22:12:16 +0800

    add screen sizes 🤳

--- landing-page/index.js
+++ landing-page/index.js
@@ -1,5 +1,10 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
+const icon = document.querySelector('.icon')
+
+icon.addEventListener('click', () => {
+    icon.classList.toggle("open");
+});
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
@@ -9,9 +14,5 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
+    icon.classList.remove("open")
 }))
-
-const icon = document.querySelector('.icon')
-icon.addEventListener('click', () => {
-    icon.classList.toggle("open");
-});
